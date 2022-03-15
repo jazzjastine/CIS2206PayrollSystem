e12adbd205060d8f685e5d471f3f1cc6639c98df
Wayne Dayata <20100215@usc.edu.ph>
2022-03-15 14:45:59 +0800

    Modified program template

--- PayrollSystem.c
+++ PayrollSystem.c
@@ -1,5 +1,47 @@
-#include<stdio.h>
+/**************************************************************
+ *
+ * CIS 2206 Practice Activity #1 - Payroll System using C
+ * Created by: Team 1
+ * Members: 
+ * Date: March 14-21, 2022
+ *  
+ * ************************************************************/
 
+
+/***** HEADER FILES TO INCLUDE *****/
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+/***** DATA STRUCTURE DEFINITION *****/
+typedef struct{
+    char LName[16];
+    char fName[24];
+    char MI;
+}nameType;
+
+char fileName[] = "EmployeeList";
+
+
+/***** FUNCTION PROTOTYPES *****/
+
+
+
+
+/***** main() function - Handles the main menu and calls the subfunction *****/
 void main(){
-    printf("\nHello world!");
+    printf("hi!");
+
+}
+
+/***** FUNCTION DEFINITIONS *****/
+
+
+/**
+ * @brief fhfhf
+ * @param  
+ * @return
+ */
+void function1(int param1,int param2){
+
 }
