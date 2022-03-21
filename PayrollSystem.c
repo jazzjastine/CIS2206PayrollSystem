e23f860c59bef40ef59c6abd796d8d223e3dee18
IzatheGreat15 <20100869@usc.edu.ph>
2022-03-21 10:07:12 +0800

    view list

--- PayrollSystem.c
+++ PayrollSystem.c
@@ -96,9 +96,22 @@ typedef struct{
     employeeInfo employee;
 }employeeTable;
 
+typedef struct node{
+    int data;
+    struct node *link;
+}nodeType, *employeeList;             // the structure for sorting the employees by department
+
 /***** FUNCTION PROTOTYPES *****/
 void initialize();          // initialize by loading existing file. If none, new file will be created
 void terminate();           // properly terminate the file by freeing all dynamic memory (attendance LL)
+void viewEmployeeList();                                              // view entire list of employees
+void sortEmployees(employeeList listEmployee[]);                      // sort employees by department
+void displayEmployees(employeeList listEmployee[], char dept);        // display employee given by a department
+
+typedef employeeInfo employeeTable[MAX];  // constant hash table
+
+/* Employee hash table declaration */
+employeeTable empTable;
 
 /***** main() function - Handles the main menu and calls the subfunction *****/
 int main(){
@@ -136,4 +149,108 @@ void func1(int args1,int args2){
     /* Variable initializations */
     /* Body */
     /* Exit/return statement, if applicable */
+}
+
+/**
+ * @brief - view entire list of employees or by department
+ * @param describe the parameters
+ * @return state what the function returns
+ */
+void viewEmployeeList(){
+    printf("\n\n---------------------------------");
+    printf("\nEmployee List");
+    printf("\n---------------------------------");
+
+    /* Employee linked list declaration */
+    employeeList listEmployee[26]; 
+    int x;
+    for(x=0; x<26;x++){
+        listEmployee[x] = NULL;
+    }
+
+    sortEmployees(listEmployee);
+    
+    for(x=0; x<26;x++){
+        employeeList trav;
+        for(trav=listEmployee[x]; trav!=NULL; trav=trav->link){
+            printf("\n%d", trav->data);
+        }
+    }
+    int choice, flag = 1;
+    
+    while(flag){
+        printf("\n[1] Display all employees");
+        printf("\n[2] Display employees of a department");
+        printf("\nEnter a number: ");
+        scanf("%d", &choice);
+        switch(choice){
+            char ch;
+            case 1:
+                //display all employees
+                flag = 0;
+                for(ch = 'A'; ch <= 'Z'; ch++){
+                    displayEmployees(listEmployee, ch);
+                }
+                break;
+            case 2:
+                //display all employees of a department
+                flag = 0;
+                do{
+                    printf("Please enter a department[A-Z]: ");
+                    scanf(" %c", &ch);
+                }while(!isalpha(ch)); 
+                displayEmployees(listEmployee, ch);
+                break;
+            default:
+                printf("Please enter 1 or 2");
+                break;
+        }
+    }
+}
+
+/**
+ * @brief - display all employees in a given department
+ * @param - list of employees, department name
+ * @return state what the function returns
+ */
+void displayEmployees(employeeList listEmployee[], char dept){
+    dept = toupper(dept);
+    printf("\nDepartment %c", dept);
+    printf("\n----------------------------------");
+    int index = dept - 'A';
+    employeeList trav, ptr;
+
+    for(trav = listEmployee[index]; trav!=NULL;){
+        employeeDetails temp = empTable[trav->data].employee;
+        printf("\n%10s %20s, %20s %10c", temp.empID, temp.name.LName, temp.name.fName, temp.name.MI);
+        ptr = trav;
+        trav = trav->link;
+        free(ptr);
+    }
+}
+
+/**
+ * @brief - sort employees by department
+ * @param - list of employees
+ * @return state what the function returns
+ */
+void sortEmployees(employeeList listEmployee[]){
+    int x;
+    attendanceHistory ptr;
+    for(x = 0; x < MAX; x++){
+        employeeInfo temp = empTable[x];
+        if(strcmp(temp.employee.empID, "EMPTY")!=0 && temp.history!=NULL){
+            printf("\n%d");
+            ptr = temp.history;
+            int index = ptr->attendance.payrollID[strlen(ptr->attendance.payrollID)-1] - 'A';
+            
+            /*insert the index of the employee to their department - sorted by empID*/
+            employeeList *trav;
+            for(trav = &listEmployee[index]; *trav!=NULL && (strcmp(empTable[(*trav)->data].employee.empID, temp.employee.empID)<0); trav = &(*trav)->link){}
+                employeeList new = calloc(1, sizeof(struct node));
+                new->data = x;
+                new->link = *trav;
+                *trav = new;
+        }
+    }
 }
